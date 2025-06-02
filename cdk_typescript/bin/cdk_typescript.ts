3168932b25b337390b931e034f4bf4815ed6e2fa
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2025-06-02 00:14:22 -0700

    Add RDS stack and update EC2 stack
    Added RDS stack with ingress rule. Updated EC2 security groups.

--- cdk_typescript/bin/cdk_typescript.ts
+++ cdk_typescript/bin/cdk_typescript.ts
@@ -3,13 +3,21 @@ import "source-map-support/register";
 import * as cdk from 'aws-cdk-lib';
 import { CdkTypescriptStack } from '../lib/cdk_typescript-stack';
 import { EC2Stack } from '../lib/ec2-stack';
+import { RDSStack } from '../lib/rds-stack';
 
 const app = new cdk.App();
 const vpcStack = new CdkTypescriptStack(app, 'CdkTypescriptStack', {
 
 });
 
-new EC2Stack(app, "MyEC2Stack", {
+const ec2Stack = new EC2Stack(app, "MyEC2Stack", {
   vpc: vpcStack.vpc,
   keyPairName: "cdk_test", // Test key pair name for SSH access
 });
+
+new RDSStack(app, "MyRDSStack", {
+  vpc: vpcStack.vpc,
+  ec2SecurityGroupAccess: ec2Stack.ec2SecurityGroup, // Security group for EC2 instance
+});
+
+app.synth();
--- cdk_typescript/lib/ec2-stack.ts
+++ cdk_typescript/lib/ec2-stack.ts
@@ -9,23 +9,30 @@ interface EC2StackProps extends cdk.StackProps {
 
 export class EC2Stack extends cdk.Stack {
 
+    public readonly ec2SecurityGroup: ec2.SecurityGroup; // Exported security group for EC2 instance
+
     constructor(scope: Construct, id: string, props: EC2StackProps) {
         super(scope, id, props);
 
 
-        // Security Group for instance in AZ1
-        const securityGroupAZ1 = new ec2.SecurityGroup(this, 'SecurityGroupAZ1', {
+        // Security Group for EC2 instances
+        const ec2SecurityGroup = new ec2.SecurityGroup(this, 'EC2SecurityGroup', {
             vpc: props.vpc,
             description: 'Security Group for EC2 Instance in AZ1',
             allowAllOutbound: true,
         });
 
         // Allow inbound SSH traffic from my IP for instance in AZ1
-        securityGroupAZ1.addIngressRule(
-            ec2.Peer.ipv4('50.98.36.55/32'),
+        ec2SecurityGroup.addIngressRule(
+            ec2.Peer.ipv4('1.1.1.1/32'), // add own IP address here
             ec2.Port.tcp(22),
             'Allow SSH access from my IP'
         );
+
+        // Exported security group for use in RDS stack
+        this.ec2SecurityGroup = ec2SecurityGroup;
+
+        cdk.Tags.of(ec2SecurityGroup).add('Name', 'Public-EC2-SG');
 
         // EC2 instance in AZ1
         const instanceAZ1 = new ec2.Instance(this, 'Public-EC2-Instance-AZ1', {
@@ -38,28 +45,13 @@ export class EC2Stack extends cdk.Stack {
                 generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
             }),
             instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO), // t2.micro 
-            securityGroup: securityGroupAZ1,
+            securityGroup: ec2SecurityGroup, // Attach defined security group to the instance
 
-            keyName: props.keyPairName, // Specify the key pair name for SSH access
+            keyName: props.keyPairName, // Allows SSH access using key pair "cdk_test"
         });   
         
-        cdk.Tags.of(securityGroupAZ1).add('Name', 'Public-EC2-SG-AZ1');
         cdk.Tags.of(instanceAZ1).add('Name', 'Public-EC2-Instance-AZ1');
 
-
-        // Security Group for instance in AZ2
-        const securityGroupAZ2 = new ec2.SecurityGroup(this, 'SecurityGroupAZ2', {
-            vpc: props.vpc,
-            description: 'Security Group for EC2 Instance in AZ2',
-            allowAllOutbound: true,
-        });
-
-        // Allow inbound SSH traffic from my IP for instance in AZ2
-        securityGroupAZ2.addIngressRule(
-            ec2.Peer.ipv4('50.98.36.55/32'),
-            ec2.Port.tcp(22),
-            'Allow SSH access from my IP'
-        );
 
         // EC2 instance in AZ2
         const instanceAZ2 = new ec2.Instance(this, 'Public-EC2-Instance-AZ2', {
@@ -72,12 +64,11 @@ export class EC2Stack extends cdk.Stack {
                 generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
             }),
             instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO), // t2.micro 
-            securityGroup: securityGroupAZ2,
-            
-            keyName: props.keyPairName, // Specify the key pair name for SSH access
+            securityGroup: ec2SecurityGroup, // Attach defined security group to the instance
+
+            keyName: props.keyPairName, // Allows SSH access using key pair "cdk_test"
         });   
         
-        cdk.Tags.of(securityGroupAZ2).add('Name', 'Public-EC2-SG-AZ2');
         cdk.Tags.of(instanceAZ2).add('Name', 'Public-EC2-Instance-AZ2');
     }
 }
